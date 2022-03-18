2712edd8724b21dfdfa5aab494db9181ad969b99
abenooo <abenezerkifle000@gmail.com>
2022-03-18 05:02:22 -0700

    isUpper checker function

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - checks if uppercase
+ *@c: character ti check
+ *
+ *Return: gives 1 or 0
+ */
+int _isupper(int c)
+{
+  if(c >= 'A' && c<= 'Z')
+    {
+     return (0);
+ }
+}
