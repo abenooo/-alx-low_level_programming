496d14ac31248a5ef2857514f4044859420ac670
abenooo <52881874+abenooo@users.noreply.github.com>
2022-03-21 19:25:48 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,34 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string
- * @str: the string to print
- *
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
 void puts2(char *str)
 {
-int i;
-int j;
-for (i = 0 ; str[i] != '\0' ; i++)
-{
-;
-}
-for (j = 0 ; j < i ; j = j + 2)
-{
-_putchar(str[j]);
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
