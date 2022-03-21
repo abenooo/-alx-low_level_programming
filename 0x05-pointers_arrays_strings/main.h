54cfbdd7ce76f02089e50a662bdd9a083848ae68
abenooo <52881874+abenooo@users.noreply.github.com>
2022-03-21 19:19:01 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts2(char *str)
+void puts2(char *str);
+void rev_string(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void puts_half(char *str);
