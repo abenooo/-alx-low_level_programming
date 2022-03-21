1fe87a5f2fef1ab9a8d498a534d791de7af29bd3
abenooo <52881874+abenooo@users.noreply.github.com>
2022-03-21 19:27:16 +0300

    update 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,31 +1,44 @@
 #include "main.h"
+
 /**
- * puts_half - prints half of a string
- * @str: the string to print half
- *
- *
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
+
 void puts_half(char *str)
 {
-int i = 0;
-int j;
-while (str[i] != '\0')
-{
-i++;
-}
-if (i % 2 == 0)
-{
-i = i / 2;
-}
-else
-{
-i = (i + 1) / 2;
-}
-while (str[i] != '\0')
-{
-j = str[i];
-i++;
-_putchar(j);
-}
-_putchar('\n');
+	int idx;
+	int len = _strlen(str);
+
+	/* find the index to start depending on even/odd amount of strlen */
+	if (len % 2 != 0)
+		idx = (len / 2) + 1;
+	else
+		idx = (len / 2);
+
+	while (idx < len)
+	{
+		_putchar(*(str + idx));
+		idx++;
+	}
+	_putchar('\n');
 }
