d27ddeba06f7184c25aaee3fab1bb19813a4d1c3
abenooo <52881874+abenooo@users.noreply.github.com>
2022-03-30 14:10:00 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,13 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
-else
-_putchar('\n');
+// if (*s)
+// {
+// _putchar(*s);
+// _puts_recursion(s + 1);
+// }
+// else
+// _putchar('\n');
+  
+   _puts_recursion("Puts with recursion");
 }
