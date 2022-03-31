204246b260541e0e08b02dd9458146ff9bdfe419
abenooo <52881874+abenooo@users.noreply.github.com>
2022-03-31 09:10:33 +0300

    update main

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,7 +1,5 @@
-int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -9,3 +7,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+int _putchar(char c);
