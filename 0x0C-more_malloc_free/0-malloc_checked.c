7c07e897bbb3269aa9d9543bded09f8c6e8bcdd6
abenooo <52881874+abenooo@users.noreply.github.com>
2022-05-11 09:17:56 +0300

    solve mallock checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,3 +1,17 @@
-#include "main."
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * malloc_checked - creates an array of integers.
+ * @b: input.
+ * Return: Output.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *g = malloc(b);
+if(g == null)
+{
+exit(98);
+}
+return (g);
+}
