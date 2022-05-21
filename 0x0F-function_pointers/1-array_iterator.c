c0e728ce31c0cc564fbe6284ca5754ebdffda7ec
abenooo <52881874+abenooo@users.noreply.github.com>
2022-05-21 12:02:12 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * array_iterator - Executes a function given as a
  *                  parameter on each element of an array.
@@ -8,12 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-  if(array == NULL || action == NUL)
-    return ;
-  while(size--> 0
-    {
-      action(*array);
-      array++;
-    }
-    }
-	
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size-- > 0)
+	{
+		action(*array);
+		array++;
+	}
+}
